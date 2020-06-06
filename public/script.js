a84e4a6e40b69711ad49eed08ba45289a9896590
Glitch (defund-nypd-reps) <none>
2020-06-06 17:53:45 +0000

    Initial commit from Glitch

---
+++ public/script.js
@@ -0,0 +1,114 @@
+const sheetData = {};
+let district = null;
+
+const autocomplete = new google.maps.places.Autocomplete(
+  document.getElementById("address-field"),
+  {
+    // https://www1.nyc.gov/assets/planning/download/pdf/data-maps/open-data/nybb_metadata.pdf?ver=18c
+    bounds: new google.maps.LatLngBounds(
+      { lat: 40.495992, lng: -74.257159 },
+      { lat: 40.915568, lng: -73.699215 }
+    ),
+    types: ["address"],
+    strictBounds: true,
+    fields: ["address_components"]
+  }
+);
+
+google.maps.event.addListener(autocomplete, "place_changed", async function() {
+  geocode();
+});
+
+document.getElementById("district-selector").onchange = async function(e) {
+  district = parseInt(e.target.value, 10);
+  document.getElementById("address-field").value = "";
+  await render(district);
+};
+
+async function geocode() {
+  if (!autocomplete.getPlace()) {
+    return;
+  }
+  status("Loading…");
+  const addressInfo = autocomplete.getPlace().address_components;
+  const q = {};
+  addressInfo.forEach(f => {
+    if (f.types.includes("street_number")) {
+      q.houseNumber = f.short_name;
+    } else if (f.types.includes("route")) {
+      q.street = f.short_name;
+    } else if (f.types.includes("postal_code")) {
+      q.zip = f.short_name;
+    }
+  });
+  const response = await fetch(
+    `/geoclient-proxy?houseNumber=${encodeURIComponent(
+      q.houseNumber
+    )}&street=${encodeURIComponent(q.street)}&zip=${encodeURIComponent(q.zip)}`
+  );
+  const responseJSON = await response.json();
+  if (responseJSON.address && responseJSON.address.cityCouncilDistrict) {
+    district = parseInt(responseJSON.address.cityCouncilDistrict, 10);
+  }
+  if (!district) {
+    status("Couldn’t find your district");
+    return;
+  }
+  document.getElementById("district-selector").value = district;
+  render(district);
+}
+
+function status(innerHTML) {
+  document.querySelector("#result").innerHTML = innerHTML;
+}
+
+async function render(district) {
+  status("Loading…");
+  if (!sheetData.byDistrict) {
+    const sheetResponse = await fetch("/sheet");
+    sheetData.byDistrict = await sheetResponse.json();
+  }
+  const info = sheetData.byDistrict[district];
+  if (info.name === "Vacant") {
+    document.querySelector("#result").outerHTML = `<div id="result">The council seat in district ${district} is currently vacant.</div>`;
+    return;
+  }
+  document.querySelector("#result").outerHTML = `<div id="result">
+    <div class="bio">
+    <div class="img"><img src="https://raw.githubusercontent.com/NewYorkCityCouncil/districts/master/thumbnails/district-${escape(
+      district
+    )}.jpg" alt="${escape(info.name)}"></div>
+    <div class="info">
+    <h3>${escape(info.name)}</h3>
+    <h4>${escape(info.borough)}, District ${escape(info.district)}</h4>
+    <p><a href="https://council.nyc.gov/district-${escape(
+      info.district
+    )}/">Council Website</a></p>
+    ${Object.entries(info.phones).map(
+      p =>
+        `<p>${escape(p[0])}: ${p[1]
+          .map(ph => `<a href="tel:${escape(ph)}">${escape(ph)}</a>`)
+          .join(", ")}</p>`
+    ).join("")}
+    <p><a href="mailto:${escape(info.email.join(","))}">Email</a>
+    </div>
+    </div>
+    <dl>
+    ${info.fields
+      .map(f => `<dt>${escape(f[0])}</dt><dd>${escape(f[1])}</dd>`)
+      .join("")}
+    </dl>
+  </div>`;
+}
+
+function escape(text) {
+  if (text === null || typeof text === "undefined") {
+    return "";
+  }
+  return text
+    .toString()
+    .replace(/&/g, "&amp;")
+    .replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;");
+}
+
---
+++ server.js
@@ -0,0 +1,89 @@
+const express = require("express");
+const fetch = require("node-fetch");
+const { GoogleSpreadsheet } = require("google-spreadsheet");
+const app = express();
+
+const spreadsheet_id = "18pWRSu58DpENABkYUJlZw1ltCPZft7KJc6lFaOZK8-s";
+const google_api_key = process.env["GOOGLE_KEY"];
+const geoclient_app_id = process.env["GEOCLIENT_APP_ID"];
+const geoclient_app_key = process.env["GEOCLIENT_APP_KEY"];
+const geoclient_url = `https://api.cityofnewyork.us/geoclient/v1/address.json?app_id=${geoclient_app_id}&app_key=${geoclient_app_key}&`;
+
+let spreadsheetData = null;
+async function getSpreadsheetData() {
+  if (spreadsheetData && new Date() - spreadsheetData.updatedAt < 30000) {
+    return spreadsheetData.data;
+  }
+  const doc = new GoogleSpreadsheet(spreadsheet_id);
+  doc.useApiKey(google_api_key);
+  await doc.loadInfo();
+  const sheet = doc.sheetsByIndex[1];
+  await sheet.loadCells();
+  const data = {};
+  const headers = [];
+  for (let c = 0; c < sheet.columnCount; c++) {
+    headers.push(sheet.getCell(1, c).value);
+  }
+  for (let r = 2; r < sheet.rowCount; r++) {
+    const district = sheet.getCell(r, 0).value
+    if (typeof district !== "number") {
+      continue;
+    }
+    const name = sheet.getCell(r, 1).value
+    if (name === "Vacant") {
+      data[district][name] = "Vacant";
+      continue;
+    }
+    data[district] = {fields: [], phones: {}}
+    for (let c = 0; c < sheet.columnCount; c++) {
+      if (["district", "name", "borough"].includes(headers[c].toLowerCase())) {
+        data[district][headers[c].toLowerCase()] = sheet.getCell(r, c).value;
+      } else if (headers[c].toLowerCase().includes("phone") && sheet.getCell(r, c).value) {
+        data[district].phones[headers[c]] = sheet.getCell(r, c).value.toString().trim().split(/[ \n]+/g);
+      } else if (headers[c].toLowerCase() == "email" && sheet.getCell(r, c).value) {
+        data[district]["email"] = sheet.getCell(r, c).value.toString().split(/[ \n]+/g);
+      } else {
+        data[district].fields.push([headers[c], sheet.getCell(r, c).value])
+      }
+    }
+  }
+  spreadsheetData = { data: data, updatedAt: new Date() };
+  return data;
+}
+
+async function geocodeAddress(q) {
+  let queryString = `houseNumber=${encodeURIComponent(
+    q.houseNumber
+  )}&street=${encodeURIComponent(q.street)}&zip=${encodeURIComponent(q.zip)}`;
+  const response = await fetch(geoclient_url + queryString);
+  const data = await response.json();
+  return data;
+}
+
+// make all the files in 'public' available
+// https://expressjs.com/en/starter/static-files.html
+app.use(express.static("public"));
+
+// https://expressjs.com/en/starter/basic-routing.html
+app.get("/", (request, response) => {
+  response.sendFile(__dirname + "/views/index.html");
+});
+
+app.get("/sheet", async (request, response) => {
+  response.json(await getSpreadsheetData());
+});
+
+app.get("/geoclient-proxy", async (request, response) => {
+  try {
+    const data = await geocodeAddress(request.query);
+    response.status(200).json(data);
+  } catch (e) {
+    response.status(500).json(e);
+  }
+});
+
+// listen for requests :)
+const listener = app.listen(process.env.PORT, () => {
+  console.log("Your app is listening on port " + listener.address().port);
+});
+
